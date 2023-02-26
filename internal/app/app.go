d5e5f091990511887d5b298d8c9c8528c8c9649a
Nonoleg17 <Nonoleg17@mail.ru>
2023-02-26 15:47:58 +0300

    update app

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,15 +2,45 @@ package app
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 	"taskService/config"
+	"taskService/internal/controller/http"
+	"taskService/internal/usecase"
+	"taskService/internal/usecase/repo"
+	"taskService/pkg/httpserver"
 	"taskService/pkg/logger"
 	"taskService/pkg/postgres"
+
+	"github.com/gin-gonic/gin"
 )
 
 func Run(cfg *config.Config) {
 	l := logger.New(cfg.Log.Level)
-	_, err := postgres.New(cfg)
+	pg, err := postgres.New(cfg)
 	if err != nil {
 		l.Fatal(fmt.Errorf("app - Run - postgres.New: %w", err))
+	}
+
+	userUsecase := usecase.NewUserCase(repo.NewUserRepo(pg))
+
+	handler := gin.New()
+	http.NewRouter(handler, l, userUsecase)
+	httpServer := httpserver.New(handler, httpserver.Port(cfg.HTTPPort))
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case s := <-interrupt:
+		l.Info("app - Run - signal: " + s.String())
+	case err = <-httpServer.Notify():
+		l.Error(fmt.Errorf("app - Run - httpServer.Notify: %w", err))
+	}
+
+	// Shutdown
+	err = httpServer.Shutdown()
+	if err != nil {
+		l.Error(fmt.Errorf("app - Run - httpServer.Shutdown: %w", err))
 	}
 }
