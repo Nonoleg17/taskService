4daf5a7d8a3e05005062e26e112513854e0f1368
okashkarov <okashkarov@anb.lan>
2023-03-07 14:36:27 +0300

    Update migrations

---
+++ config/migrations/1_init.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+CREATE TABLE IF NOT EXISTS "users" (
+        id bigserial PRiMARY KEY,
+        username varchar unique NOT NULL,
+        email varchar NOT NULL,
+        password varchar NOT NULL
+);
+
+
+-- +goose Down
+DROP TABLE users;
--- config/migrations/20230226113759_add_users_table.down.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "users" {
-    "id" bigserial PROMARY KEY
-    "username" varchar NOT NULL,
-    "email" varchar NOT NULL,
-    "password" varchar NOT NULL,
-}
